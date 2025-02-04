2ec646d54efc5b12c108174e49f9b09f0671e5b8
Joke Heyndels <joke.heyndels@codeinstitute.net>
2025-02-04 11:34:46 +0000

    Add changes for VS Code migration and deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -64,7 +64,7 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     'localhost',
-    '8000-bravoalpha79-drfapi-nl5b6v6013p.ws.codeinstitute-ide.net',
+    '127.0.0.1',
     os.environ.get('ALLOWED_HOST'),
 ]
 
@@ -110,14 +110,14 @@ MIDDLEWARE = [
 ]
 
 
+CORS_ALLOWED_ORIGINS = []
+
 if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https:\/\/.*\.codeinstitute-ide\.net$",
-    ]
+    CORS_ALLOWED_ORIGINS.append(os.environ.get('CLIENT_ORIGIN'))
+    
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    CORS_ALLOWED_ORIGINS.append(os.environ.get('CLIENT_ORIGIN_DEV'))
 
 CORS_ALLOW_CREDENTIALS = True
 
@@ -150,7 +150,7 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DEV' in os.environ:
-     DATABASES = {
+    DATABASES = {
         'default': {
              'ENGINE': 'django.db.backends.sqlite3',
              'NAME': BASE_DIR / 'db.sqlite3',
