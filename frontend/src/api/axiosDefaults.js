a8ab8cc8bdd920ea3c0a91216c8a87cf4c4c721b
Joke Heyndels <joke.heyndels@codeinstitute.net>
2025-02-20 10:29:51 +0000

    Update settings, urls and axios default base url to allow app to run with single command

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://jo-drf-migration-c1d05b0e9042.herokuapp.com/";
+axios.defaults.baseURL = "api/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
