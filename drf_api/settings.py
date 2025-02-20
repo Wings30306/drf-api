180088a07ca4caafa61b42f98e7f5334ebf008b2
Joke Heyndels <joke.heyndels@codeinstitute.net>
2025-02-20 10:52:58 +0000

    Prepare for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -189,7 +189,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
 
